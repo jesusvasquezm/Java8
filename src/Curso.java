5fe39fe5f20bafff5c3f09882a8a27e7556a28e6
Jesus Vasquez Medina <jesusvasquezm098@gmail.com>
2023-08-08 10:55:31 -0300

    Conheçendo mais sobre o Stream, Collectors, APIs e clase Optional

--- src/Curso.java
+++ src/Curso.java
@@ -1,14 +1,13 @@
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-public class Cursos {
+public class Curso {
 
     private String nome;
     private int alunos;
 
-    public Cursos(String nome, int alunos) {
+    public Curso(String nome, int alunos) {
         this.nome = nome;
         this.alunos = alunos;
     }
@@ -23,11 +22,11 @@ public class Cursos {
 
 
     public static void main(String[] args) {
-        List<Cursos> cursos = new ArrayList<Cursos>();
-        cursos.add(new Cursos("Python", 45));
-        cursos.add(new Cursos("JavaScript", 150));
-        cursos.add(new Cursos("Java 8", 113));
-        cursos.add(new Cursos("C", 55));
+        List<Curso> cursos = new ArrayList<Curso>();
+        cursos.add(new Curso("Python", 45));
+        cursos.add(new Curso("JavaScript", 150));
+        cursos.add(new Curso("Java 8", 113));
+        cursos.add(new Curso("C", 55));
 
         //ordenar essa lista pela quantidade de alunos
         //com lambda
@@ -36,7 +35,7 @@ public class Cursos {
         System.out.println("==========");
 
         //com method reference
-        cursos.sort(Comparator.comparing(Cursos::getAlunos));
+        cursos.sort(Comparator.comparing(Curso::getAlunos));
         cursos.forEach(System.out::println); //imprime uma lista com o id de cada objeto e não o "resultado" em si
         System.out.println("==========");
 
@@ -45,7 +44,7 @@ public class Cursos {
         System.out.println("==========");
 
         //Como transformar o nosso Stream<Curso> em um Stream<String> contendo apenas os nomes dos cursos?
-        Stream<String> nomes = cursos.stream().map(Cursos::getNome);
+        Stream<String> nomes = cursos.stream().map(Curso::getNome);
 
         //Usando lambda
         cursos.stream()
@@ -56,9 +55,26 @@ public class Cursos {
         //Usando method reference
         cursos.stream()
                 .filter(c -> c.getAlunos() > 50)
-                .map(Cursos::getAlunos)
+                .map(Curso::getAlunos)
                 .forEach(System.out::println);
 
+        //Pegar o primeiro elemento com o Stream
+        //Optional<Cursos> first =
+                cursos.stream().filter(c -> c.getAlunos() > 50).findFirst();
+
+        // a quantidade média de alunos de todos os seus cursos utilizando a API de Stream.
+        //OptionalDouble average =
+                cursos.stream().mapToInt(c -> c.getAlunos()).average();
+
+        //Como podemos transformar esse Stream<Curso> filtrado em uma List<Curso>?
+
+        Stream<Curso> stream = cursos.stream()
+                .filter(c -> c.getAlunos() > 50);
+
+        //guardado em uma lista
+        List<Curso> collect = cursos.stream()
+                .filter(c -> c.getAlunos() > 50)
+                .collect(Collectors.toList());
     }
 }
 
